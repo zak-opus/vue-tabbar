4e15c1501ffb1154b74b4e04a34a677947d6e4f3
zak-opus <akak7@foxmail.com>
2019-11-08 17:17:42 +0800

    第一次初始化

---
+++ src/components/tabbar/TabBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="tab-bar">
+      <div id="tab-bar-item">首页</div>
+      <div id="tab-bar-item">分类</div>
+      <div id="tab-bar-item">购物车</div>
+      <div id="tab-bar-item">我的</div>
+  </div>
+</template>
+<script>
+export default {
+  name:"TabBar",
+}
+</script>
+<style>
+#tab-bar{
+  display: flex;
+  background-color: #eee;
+  position: fixed;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  box-shadow: 0 -1px 1px rgba(100,100,100, .2)
+}
+#tab-bar-item{
+  flex: 1;
+  text-align: center;
+  height: 49px;
+}
+</style>
