3d22af000e3f6906141c2c225c83e7813bdff97d
zak-opus <akak7@foxmail.com>
2019-11-08 19:30:59 +0800

    通过props传值，完成item与路由切换

--- src/components/tabbar/TabBar.vue
+++ src/components/tabbar/TabBar.vue
@@ -1,17 +1,14 @@
 <template>
   <div id="tab-bar">
-      <div id="tab-bar-item">首页</div>
-      <div id="tab-bar-item">分类</div>
-      <div id="tab-bar-item">购物车</div>
-      <div id="tab-bar-item">我的</div>
+    <slot></slot>
   </div>
 </template>
 <script>
 export default {
-  name:"TabBar",
+  name:"TabBar"
 }
 </script>
-<style>
+<style scoped>
 #tab-bar{
   display: flex;
   background-color: #eee;
@@ -20,10 +17,5 @@ export default {
   right: 0px;
   bottom: 0px;
   box-shadow: 0 -1px 1px rgba(100,100,100, .2)
-}
-#tab-bar-item{
-  flex: 1;
-  text-align: center;
-  height: 49px;
 }
 </style>
